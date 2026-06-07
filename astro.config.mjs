540ca266331ae02933cf2d0c0a306b0f1268f2e1
Hovaness Bartamian <hovanessb@users.noreply.github.com>
2026-06-07 00:15:20 -0400

    Refactor site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://thebarblueprint.net',
+  compressHTML: true,
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Karla',
+      cssVariable: '--font-karla',
+      weights: [300, 400, 500, 600, 700],
+      styles: ['normal', 'italic'],
+      subsets: ['latin'],
+    },
+    {
+      provider: fontProviders.google(),
+      name: 'Playfair Display',
+      cssVariable: '--font-playfair',
+      weights: [400, 600, 700],
+      styles: ['normal', 'italic'],
+      subsets: ['latin'],
+    },
+    {
+      provider: fontProviders.google(),
+      name: 'Playfair Display SC',
+      cssVariable: '--font-playfair-sc',
+      weights: [400, 700],
+      styles: ['normal'],
+      subsets: ['latin'],
+    },
+  ],
+});
